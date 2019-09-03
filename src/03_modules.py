f58ecb7bd5f3e90d18290740a52461813cf4fe27
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-09-03 14:11:42 -0700

    Figured out how to run a file without vs code debugger

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,8 +10,10 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+print("this is the script location and name: " + sys.argv[0])
 
 # Print out the OS platform you're using:
+# print("My OS Version is" + sys)
 # YOUR CODE HERE
 
 # Print out the version of Python you're using:
