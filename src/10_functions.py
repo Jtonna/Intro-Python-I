c1bfa14d4281c62cea9904c8fbc3780f85477bcc
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-09-04 13:06:27 -0700

    Update 10_functions.py

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,4 +1,9 @@
 # Write a function is_even that will return true if the passed-in number is even.
+def is_even(num):
+	if num % 2 == 0:
+		return "Even!"
+	else:
+		return "Odd!"
 
 # YOUR CODE HERE
 
@@ -9,4 +14,4 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+print(is_even(num))
