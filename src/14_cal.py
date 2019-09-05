23909ed709a152349de2a8f1fd54596d11f87a91
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-09-04 18:58:21 -0700

    Fixed some duki formatting, super lost with these instructions though. They make no sense

--- src/14_cal.py
+++ src/14_cal.py
@@ -5,20 +5,28 @@ https://docs.python.org/3.6/library/calendar.html
 
 Write a program that accepts user input of the form
   `14_cal.py month [year]`
-and does the following:
- - If the user doesn't specify any input, your program should
-   print the calendar for the current month. The 'datetime'
-   module may be helpful for this.
- - If the user specifies one argument, assume they passed in a
-   month and render the calendar for that month of the current year.
- - If the user specifies two arguments, assume they passed in
-   both the month and the year. Render the calendar for that
-   month and year.
- - Otherwise, print a usage statement to the terminal indicating
-   the format that your program expects arguments to be given.
-   Then exit the program.
+
+  and does the following:
+
+  - If the user doesn't specify any input, your program should
+    print the calendar for the current month. The 'datetime'
+    module may be helpful for this.
+  
+  - If the user specifies one argument, assume they passed in a
+    month and render the calendar for that month of the current year.
+  
+  - If the user specifies two arguments, assume they passed in
+    both the month and the year. Render the calendar for that
+    month and year.
+  
+  - Otherwise, print a usage statement to the terminal indicating
+    the format that your program expects arguments to be given.
+    Then exit the program.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+
+user_input = input("Please enter a date in the format of []")
