47eec12a9d9d92ae156e7958e63468136bffb213
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-09-05 15:52:27 -0700

    Think ive got it now

--- src/14_cal.py
+++ src/14_cal.py
@@ -27,3 +27,28 @@ Write a program that accepts user input of the form
 import sys
 import calendar
 from datetime import datetime
+
+# If the user doesnt pass any argument in it just returns the calendar for their current year and month
+if len(sys.argv) == 1:
+  theMonth = datetime.now().month
+  theYear = datetime.now().year
+  print('t1' + calendar.month(theYear, theMonth)) 
+  quit()
+
+# If the user passes in the month, we shall assume the year they want is the current calendar year
+elif len(sys.argv) == 2:
+  theMonth = int(sys.argv[1])
+  theYear = datetime.now().year
+  print('t2' + calendar.month(theYear, theMonth))
+  quit()
+
+# If the user passes in a month and year, we give them what they want.
+elif len(sys.argv) == 3:
+  theMonth = int(sys.argv[1])
+  theYear = int(sys.argv[2])
+  print('t3' + calendar.month(theYear, theMonth))
+  quit()
+
+# If the user passes in more than 3 arguments we give them a usage statement
+elif len(sys.argv) > 3:
+  print("****USAGE**** \n This program will do one of three things \n 1. If it is ran it will return the current calendar for your month & year \n 2. It will accept you passing in a month in the format of [01] without brackets, and will assume the year you want is the current year \n 3. you can pass in a month in the format of [01] and a year in the format of [1999]. ex [02 1999] but without brackets. It will return the celandar for the respective monthand year. ")
